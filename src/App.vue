8fb977e58ff958177319db087690285f208338cd
huangxiaogeng <986649825@qq.com>
2020-07-31 20:17:37 +0800

    css优化

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,10 @@
 					
 					<div class=" ServiceRight FlexTwo " style="background-color:#FFFFFF;height: 100%; position: relative;">
 						<MTitle></MTitle>
-						<Current :title="'123'" :money="'hell'" ></Current>
+						
+						
+							<Current :title="'123'" :money="'hell'"  v-for=""></Current>
+						
 					     <div style="display: flex;">
 							 <LineChart>						 
 							 </LineChart>
@@ -63,6 +66,14 @@
 				 content="hello" money="-12"></Transaction>
 				<ButtonI style="margin: 0.625rem;"></ButtonI>
 			
+			   <div class="circleDiv">
+				   
+				   <img class="circle
+				   " src="./assets/logo.png">
+					   
+				   </img>
+			   </div>
+			  
 			</div>
 			
 			
@@ -81,6 +92,7 @@
 	import Current from "./components/app/Current.vue"
 	import LineChart from "./components/app/lineChart.vue"
 	import FlexRow from "./components/app/FlexRow.vue"
+	import Block from "./components/app/Block.vue"
 	export default {
 		name: 'app',
 		components: {
@@ -93,7 +105,8 @@
 			MTitle,
 			Current,
 			LineChart,
-			FlexRow
+			FlexRow,
+			Block
 		},
 		data: function() {
 			return {
@@ -134,6 +147,7 @@
 		min-height: 54rem;
 		min-width: 84rem;
 		
+		
 
 	}
 
@@ -144,7 +158,7 @@
 		background-color: white;
 		border-radius: 1.5rem;
 		-moz-box-shadow: 10px 10px 5px #888888; /* 老的 Firefox */
-		box-shadow: 10px 10px 5px #888888;
+		box-shadow:0px 0px 10px #000;
 		
 	}
 
@@ -160,8 +174,9 @@
 		background-color: white;
 		border-radius: 1rem;
 		text-align: center;
-		border: 1px #000000 solid;
+		/* border: 1px #000000 solid; */
 		margin-left: 0.625rem;
+		box-shadow:0px 0px 10px #888888;
 		/* display: flex; */
 		/* align-items: center; */
 
@@ -184,7 +199,7 @@
 	 */
 	.leftMain {
 		display: flex;
-
+     padding-top: 1.875rem;
 
 	}
 
@@ -214,4 +229,23 @@
 	}
 
 	.card Card {}
+	.circleDiv{
+		display: flex;
+		flex-direction: row-reverse;
+		/* height: 12.5rem; */
+		/*  */
+		position: relative;
+		bottom: 0.625rem;
+		margin-top:5.25rem;
+	}
+	 .circleDiv .circle{
+		 position: relative;
+		width: 2.5rem;
+		height: 2.5rem;
+		border-radius: 50%;
+		overflow: hidden;
+		margin-right: 1.25rem;
+		bottom: 0.625rem;
+		
+	}
 </style>
